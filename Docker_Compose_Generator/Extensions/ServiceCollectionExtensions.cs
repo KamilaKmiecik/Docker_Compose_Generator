272e7125c3e52876133eced3c5b173fa903df97e
Kamila <kamilakmiecik01@gmail.com>
2024-10-13 14:13:50 +0200

    Added extension method

---
+++ Docker_Compose_Generator/Extensions/ServiceCollectionExtensions.cs
@@ -0,0 +1,13 @@
+﻿using Docker_Compose_Generator.Services;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace Docker_Compose_Generator.Extensions;
+    public static class ServiceCollectionExtensions
+    {
+        public static IServiceCollection AddServices(this IServiceCollection services)
+        {
+            services.AddScoped<IDockerComposeService, DockerComposeService>();
+
+            return services; 
+        }
+    }
