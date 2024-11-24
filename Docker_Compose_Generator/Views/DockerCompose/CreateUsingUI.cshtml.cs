5acedd15418627c195beb97eed97d29acad9387d
Kamila <kamilakmiecik01@gmail.com>
2024-11-24 19:33:51 +0100

    ui fixes/changes

---
+++ Docker_Compose_Generator/Views/DockerCompose/CreateUsingUI.cshtml.cs
@@ -0,0 +1,12 @@
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+
+namespace Docker_Compose_Generator.Views.DockerCompose
+{
+    public class CreateUsingUIModel : PageModel
+    {
+        public void OnGet()
+        {
+        }
+    }
+}
